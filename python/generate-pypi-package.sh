464e50bebc0250d4b9a011ba75a7f334583058e3
Gaute Lindkvist <lindkvis@gmail.com>
2020-06-16 12:30:07 +0200

    Tidy up package generation scripts

--- python/generate-pypi-package.sh
+++ python/generate-pypi-package.sh
@@ -13,10 +13,11 @@ git checkout system-python-pypi
 mkdir build && cd build
 cmake3 -DPYTHON_EXECUTABLE=/usr/bin/python3 -DBOOST_INCLUDEDIR=/usr/include/boost169 -DBOOST_LIBRARYDIR=/usr/lib64/boost169 \
 -DOPM_ENABLE_PYTHON=ON -DOPM_ENABLE_DYNAMIC_BOOST=OFF -DOPM_ENABLE_DYNAMIC_PYTHON_LINKING=OFF ..
-# make step is necessary until the generated ParserKeywords/*.hpp
-# are generated in the Python step
+
+# make step is necessary until the generated ParserKeywords/*.hpp are generated in the Python step
 make -j2
-rm -rf dist
+
+# Run setup-package.sh three times (Python 3.6, 3.7 and 3.8)
 ./setup-package.sh
 cmake3 -DPYTHON_EXECUTABLE=/usr/local/bin/python3.7 -DBOOST_INCLUDEDIR=/usr/include/boost169 -DBOOST_LIBRARYDIR=/usr/lib64/boost169 \
 -DOPM_ENABLE_PYTHON=ON -DOPM_ENABLE_DYNAMIC_BOOST=OFF -DOPM_ENABLE_DYNAMIC_PYTHON_LINKING=OFF ..
@@ -24,3 +25,8 @@ cmake3 -DPYTHON_EXECUTABLE=/usr/local/bin/python3.7 -DBOOST_INCLUDEDIR=/usr/incl
 cmake3 -DPYTHON_EXECUTABLE=/usr/local/bin/python3.8 -DBOOST_INCLUDEDIR=/usr/include/boost169 -DBOOST_LIBRARYDIR=/usr/lib64/boost169 \
 -DOPM_ENABLE_PYTHON=ON -DOPM_ENABLE_DYNAMIC_BOOST=OFF -DOPM_ENABLE_DYNAMIC_PYTHON_LINKING=OFF ..
 ./setup-package.sh
+
+/usr/bin/python3 -m auditwheel repair dist/*.whl
+
+# Example of upload
+# /usr/bin/python3 -m twine upload --repository testpypi wheelhouse/*
