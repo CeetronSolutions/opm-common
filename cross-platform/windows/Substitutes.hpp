e3bfaf19488371126471cb89f9ba2fb5aacbe3b4
rubenthoms <ruben.thoms@ceetronsolutions.com>
2020-08-24 13:35:02 +0200

    Moved function definitions from source to header file.

--- cross-platform/windows/Substitutes.hpp
+++ cross-platform/windows/Substitutes.hpp
@@ -1,7 +1,37 @@
 #pragma once
+#ifdef _WIN32
 
-#define FNM_PATHNAME (1 << 0) /* No wildcard can ever match '/'.  */
-#define FNM_NOESCAPE (1 << 1) /* Backslashes don't quote special chars.  */
-#define FNM_PERIOD (1 << 2) /* Leading '.' is matched only explicitly.  */
+#include <Shlwapi.h>
+#define NOMINMAX
+#include <windows.h>
+#undef NOMINMAX
+// IGNORE is used in InputErrorAction.hpp, as an enum value, 
+// but is defined as a #def in WinBase.h which is included by Shlwapi.h.
+// It is not required here, so we can undefine it.
+#undef IGNORE
+#undef GROUP_NAME
+#undef ERROR
+#include <iostream>
+#include <stddef.h>
 
-int fnmatch(const char* pattern, const char* string, int flags);
+#if defined(_MSC_VER)
+#include <BaseTsd.h>
+typedef SSIZE_T ssize_t;
+#endif
+
+int fnmatch(const char* pattern, const char* string, int flags)
+{
+    if (flags != 0) {
+        std::cerr << __FUNCTION__ << "Warning: flags other than 0 are not supported in Windows.";
+    }
+    wchar_t pszFile[1024];
+    wchar_t pszSpec[1024];
+
+    size_t outsize;
+    mbstowcs_s(&outsize, pszFile, string, strlen(string) + 1);
+    mbstowcs_s(&outsize, pszSpec, pattern, strlen(pattern) + 1);
+
+    return (!PathMatchSpecW(pszFile, pszSpec));
+}
+
+#endif
