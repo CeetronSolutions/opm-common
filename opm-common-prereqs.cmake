d84d7888c780974f168558a1d540ba438a63fbc7
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2022-07-05 08:43:51 +0200

    Avoid boost unit_test_framework

--- opm-common-prereqs.cmake
+++ opm-common-prereqs.cmake
@@ -12,14 +12,30 @@ set (opm-common_DEPS
 	"C99"
 )
 
-list(APPEND opm-common_DEPS
-      # various runtime library enhancements
-      "Boost 1.44.0 COMPONENTS system unit_test_framework REQUIRED"
-      "OpenMP QUIET"
-      "cjson"
-      # Still it produces compile errors complaining that it
-      # cannot format UDQVarType. Hence we use the same version
-      # as the embedded one.
-      "fmt 7.0.3"
-)
+if(BUILD_TEST_FRAMEWORK)
+	list(APPEND opm-common_DEPS
+		# various runtime library enhancements
+		"Boost 1.44.0 COMPONENTS system unit_test_framework REQUIRED"
+		"OpenMP QUIET"
+		"cjson"
+
+		# Still it produces compile errors complaining that it
+		# cannot format UDQVarType. Hence we use the same version
+		# as the embedded one.
+		"fmt 7.0.3"
+	)
+else()
+	list(APPEND opm-common_DEPS
+		# various runtime library enhancements
+		"Boost 1.44.0 COMPONENTS system REQUIRED"
+		"OpenMP QUIET"
+		"cjson"
+
+		# Still it produces compile errors complaining that it
+		# cannot format UDQVarType. Hence we use the same version
+		# as the embedded one.
+		"fmt 7.0.3"
+	)
+endif()
+
 find_package_deps(opm-common)
