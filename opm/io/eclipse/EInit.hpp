354375b8ae55fad91efa64a298de4e0871defd19
jonjenssen <jon@soundsoft.no>
2024-06-18 17:05:56 +0200

    Support dual poro in init files, too

--- opm/io/eclipse/EInit.hpp
+++ opm/io/eclipse/EInit.hpp
@@ -25,50 +25,53 @@
 #include <vector>
 #include <map>
 
-namespace Opm { namespace EclIO {
+namespace Opm {
+    namespace EclIO {
 
-class EInit : public EclFile
-{
-public:
-    explicit EInit(const std::string& filename);
+        class EInit : public EclFile
+        {
+        public:
+            explicit EInit(const std::string& filename);
 
-    const std::vector<std::string>& list_of_lgrs() const { return lgr_names; }
+            const std::vector<std::string>& list_of_lgrs() const { return lgr_names; }
 
-    std::vector<EclFile::EclEntry> list_arrays() const;
-    std::vector<EclFile::EclEntry> list_arrays(const std::string& grid_name) const;
+            std::vector<EclFile::EclEntry> list_arrays() const;
+            std::vector<EclFile::EclEntry> list_arrays(const std::string& grid_name) const;
 
-    const std::array<int, 3>& grid_dimension(const std::string& grid_name = "global") const;
-    int activeCells(const std::string& grid_name = "global") const;
+            const std::array<int, 3>& grid_dimension(const std::string& grid_name = "global") const;
+            int activeCells(const std::string& grid_name = "global") const;
 
-    bool hasLGR(const std::string& name) const;
+            bool hasLGR(const std::string& name) const;
 
-    template <typename T>
-    const std::vector<T>& getInitData(const std::string& name, const std::string& grid_name = "global")
-    {
-        return this->ImplgetInitData<T>(name, grid_name);
+            template <typename T>
+            const std::vector<T>& getInitData(const std::string& name, const std::string& grid_name = "global")
+            {
+                return this->ImplgetInitData<T>(name, grid_name);
+            }
+
+        protected:
+
+            template <typename T>
+            const std::vector<T>& ImplgetInitData(const std::string& name, const std::string& grid_name = "global");
+
+        private:
+            std::array<int, 3> global_nijk;
+            std::vector<std::array<int, 3>> lgr_nijk;
+
+            int global_nactive;
+            std::vector<int> lgr_nactive;
+            bool dual_porosity;
+
+            std::vector<std::string> lgr_names;
+
+            std::map<std::string, int> global_array_index;
+            std::vector<std::map<std::string, int>> lgr_array_index;
+
+            int get_array_index(const std::string& name, const std::string& grid_name) const;
+            int get_lgr_index(const std::string& grid_name) const;
+        };
+
     }
-
-protected:
-
-    template <typename T>
-    const std::vector<T>& ImplgetInitData(const std::string& name, const std::string& grid_name = "global");
-
-private:
-    std::array<int, 3> global_nijk;
-    std::vector<std::array<int, 3>> lgr_nijk;
-
-    int global_nactive;
-    std::vector<int> lgr_nactive;
-
-    std::vector<std::string> lgr_names;
-
-    std::map<std::string,int> global_array_index;
-    std::vector<std::map<std::string,int>> lgr_array_index;
-
-    int get_array_index(const std::string& name, const std::string& grid_name) const;
-    int get_lgr_index(const std::string& grid_name) const;
-};
-
-}} // namespace Opm::EclIO
+} // namespace Opm::EclIO
 
 #endif // OPM_IO_EINIT_HPP
